768d6fe96e209b1e2df632eb773d6b7ffd53d143
Victor Seger <vicktor.junior@gmail.com>
2018-06-10 16:39:58 -0300

    produto e associação many to many com categoria

---
+++ src/main/java/com/victorseger/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.victorseger.cursomc.repositories;
+
+import com.victorseger.cursomc.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
