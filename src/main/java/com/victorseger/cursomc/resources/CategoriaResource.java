7688e62ecafbf05fe4272d6ee7b20d0649aedb10
Victor Seger <vicktor.junior@gmail.com>
2018-06-13 00:00:28 -0300

    deletando categoria com DELETE

--- src/main/java/com/victorseger/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/victorseger/cursomc/resources/CategoriaResource.java
@@ -39,6 +39,12 @@ public class CategoriaResource {
         categoria.setId(id);
         categoria = service.update(categoria);
         return ResponseEntity.noContent().build();
-
     }
+
+    @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/com/victorseger/cursomc/services/CategoriaService.java
+++ src/main/java/com/victorseger/cursomc/services/CategoriaService.java
@@ -2,8 +2,10 @@ package com.victorseger.cursomc.services;
 
 import com.victorseger.cursomc.domain.Categoria;
 import com.victorseger.cursomc.repositories.CategoriaRepository;
+import com.victorseger.cursomc.services.exceptions.DataIntegrityException;
 import com.victorseger.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -23,8 +25,6 @@ public class CategoriaService {
                 "Objeto não encontrado! Id: " + ", Tipo: " + Categoria.class.getName()));
     }
 
-
-
     public Categoria insert(Categoria categoria) {
         categoria.setId(null);
         return repo.save(categoria);
@@ -33,5 +33,15 @@ public class CategoriaService {
     public Categoria update(Categoria categoria) {
         find(categoria.getId());
         return repo.save(categoria);
+    }
+
+    public void delete(Integer id) {
+        find(id);
+        try {
+            repo.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir categoria que possui produtos");
+        }
+
     }
 }
