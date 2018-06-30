f0390ac0f52cb1308715673a1abe9dcbf8f4feba
Victor Seger <vicktor.junior@gmail.com>
2018-06-30 19:21:54 -0300

    login com refresh token

---
+++ src/main/java/com/victorseger/cursomc/resources/AuthResource.java
@@ -0,0 +1,31 @@
+package com.victorseger.cursomc.resources;
+
+import com.victorseger.cursomc.security.JWTUtil;
+import com.victorseger.cursomc.security.UserSS;
+import com.victorseger.cursomc.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthResource {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+
+    //método para atualizar o token uma vez que o login permanece
+    @RequestMapping(value="/refresh_token", method= RequestMethod.POST)
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+        UserSS user = UserService.authenticated();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+
+}
