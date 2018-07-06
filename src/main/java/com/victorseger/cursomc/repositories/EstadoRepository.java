4ac8015687524883889e5632f4582caadaf922c6
Victor Seger <vicktor.junior@gmail.com>
2018-07-06 20:57:36 -0300

    endpoint para estados e cidades

--- src/main/java/com/victorseger/cursomc/repositories/EstadoRepository.java
+++ src/main/java/com/victorseger/cursomc/repositories/EstadoRepository.java
@@ -3,7 +3,16 @@ package com.victorseger.cursomc.repositories;
 import com.victorseger.cursomc.domain.Estado;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+    @Transactional(readOnly = true)
+    public List<Estado> findAllByOrderByNome(); // retorna todos os estados ordenados por nome
+
+
+
 }
